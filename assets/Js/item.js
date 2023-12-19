63ef3ee334f108232f36984c7d21afd34f116405
kelvinhsieh28 <kelvinhsieh28@gmail.com>
2023-12-19 16:34:48 +0800

    第六次推送

--- assets/Js/item.js
+++ assets/Js/item.js
@@ -1,33 +1,15 @@
-let slideIndex = 1; //先設預設變數為1
-
-function showSlides(props) {
-  let i;
-  //取得所有的mySlides 因為他是取class 而不是id 所以要用getElementsByClassName
-  let slides = document.getElementsByClassName("singleSlide");
-
-  //如果n大於slides的長度，代表已經到最後一張，所以要從第一張開始
-  if (props > slides.length) {
-    slideIndex = 1;
-  }
-
-  if (props < 1) {
-    slideIndex = slides.length;
-  }
-
-  //將所有圖片隱藏
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-
-  //將新的圖片顯示
-  //   console.log(slideIndex - 1, 25);
-
-  //array都從0開始，所以要減一，因為我們的slideIndex是從1開始 ，已經賦予值
-  slides[slideIndex - 1].style.display = "flex";
+function minus(){
+  var number = Number (document.getElementsByClassName("item_num")[0].value);
+  if (number > 1){
+      number -= 1;
+      document.getElementsByClassName("item_num")[0].value = number;
+  }else window.alert("商品數量不得少於1");
 }
 
-function plusSlides(a) {
-  showSlides((slideIndex += a));
+function add(){
+  var number = Number (document.getElementsByClassName("item_num")[0].value);
+  if (number < 100){
+      number += 1;
+      document.getElementsByClassName("item_num")[0].value = number;
+  }else window.alert("商品數量不得超過100");
 }
-//使用者點擊左右按鈕時，並傳入參數1或-1，代表下一張或上一張
-plusSlides(slideIndex);
