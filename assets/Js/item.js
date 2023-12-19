0cff3e35bf16b9060ac4d3f1a9703c6c1685fbba
kelvinhsieh28 <kelvinhsieh28@gmail.com>
2023-12-20 03:20:57 +0800

    第七次推送

--- assets/Js/item.js
+++ assets/Js/item.js
@@ -1,3 +1,25 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("singleSlide");
+  if (n > slides.length) { slideIndex = 1; }
+  if (n < 1) { slideIndex = slides.length; }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+}
+
 function minus(){
   var number = Number (document.getElementsByClassName("item_num")[0].value);
   if (number > 1){
